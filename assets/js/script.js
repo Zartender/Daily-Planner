add6591056a43698a5367034376e26dbbd7090c0
Ian Greenblott <imgreenblott@gmail.com>
2021-10-14 22:48:36 -0400

    Updating root files. Arranging file structure. injecting Jquery y javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
+
+console.log("Loading JS ");
 var today = moment();
 $("#today").text(today.format("MMM Do, YYYY"));
 console.log(today.format);
+
+$('.saveBtn').on('click', function () {
+    console.log('.saveBtn', $(this));
+    var description = $(this).siblings('.description').val();
+    console.log(description);
+    var time = $(this).siblings('.hour').text();
+    console.log(time);
+    //TO set 
+    localStorage.setItem(time, description);
+
+
+
+})
+
+// localStorage.getItem(key); to get.
+
+//.each loop 
+$(".row").each(function (index) {
+    console.log(index + ": ");
+
+    var time = $(this).find('.hour').text();
+    //grab value from LS 
+    var savedvalue = localStorage.getItem(time);
+    console.log(savedvalue);
+    //retains information in the textarea of the HTML page 
+    $(this).find('.description').val(savedvalue);
+
+});
